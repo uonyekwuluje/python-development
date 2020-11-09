5e636a7977c9009a1bd63236c7af90f45a804f50
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-09 13:27:16 -0500

    updating python code

---
+++ interview-problems/sortedArrayRemoveDublicates.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        newDict = {}  
+        for x in range(len(nums)):
+            if nums[x] not in newDict:
+                newDict[nums[x]] = 1
+            else:
+                newDict[nums[x]] = 1 
+
+        retVal = [] 
+        for x,y in newDict.items():
+            retVal.append(x)  
+
+        return (len(retVal),retVal)
+        #return len(retVal), "nums = retVal  
+
+if __name__ == "__main__":
+    #nums = [0,0,1,1,1,2,2,3,3,4]
+    nums = [1,1,2]
+    newSolution = Solution()
+    print(newSolution.removeDuplicates(nums))
+    #print("List => {} | Return => {}\n".format(nums,newSolution.removeDuplicates(nums)))
+
